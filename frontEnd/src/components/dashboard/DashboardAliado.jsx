425479e08c7f60b3c54eb8dc1cff20001528ee70
Camilo <bermeocamilo12@gmail.com>
2026-03-24 20:31:57 -0500

    fix(layout): design div for serch camp and header of the section

--- frontEnd/src/components/dashboard/DashboardAliado.jsx
+++ frontEnd/src/components/dashboard/DashboardAliado.jsx
@@ -4,16 +4,13 @@ import HederDashboardAliado from "../layout/HederDashboardAliado";
 
 const DashboardAliado = () => {
     return (
-        <div className="flex min-h-screen bg-gray-50 h-full w-full">
- 
+        <div className="flex min-h-screen  h-full w-full">
             <Sidebar />
 
-            <main className="flex-1 flex flex-col min-w-0"style={{ marginLeft: '16rem' }}>
+            <main className="flex-1 flex  flex-col min-w-0"style={{ marginLeft: '16rem' }}>
                 <div className="p-8 ">
                     <HederDashboardAliado />
-                    <div className="mt-10 p-6 bg-white rounded-xl shadow-sm border border-gray-200">
-                        <p className="text-gray-500">Contenido del panel activo...</p>
-                    </div>
+                    
                 </div>
             </main>
         </div>
--- frontEnd/src/components/layout/HederDashboardAliado.jsx
+++ frontEnd/src/components/layout/HederDashboardAliado.jsx
@@ -14,16 +14,16 @@ const HederDashboardAliado = () => {
     };
 
     return (
-        <div className="flex justify-between items-center gap-8 bg-white h-full w-full px-6 ">
+        <div className="flex justify-around rounded-2xl items-center gap-8   px-6 ">
             <div className="flex ">
                 <h1 className="text-2xl font-bold">Dashboard</h1>
             </div>
 
 
-            <div className="flex flex-1 mx-10 max-w-md bg-white rounded-3xl px-4 py-1 border border-gray-200">
-                <button><img src={BuscarDashboard} alt="buscar" className="w-5 h-5" /></button>
+            <div className="flex flex-1 mx-10 gap-2 max-w-md bg-white rounded-3xl px-4 py-1 border border-gray-200">
+                <button className="cursor-pointer"><img src={BuscarDashboard} alt="buscar" className="w-5 h-5" /></button>
                 <input
-                    className="flex-1 outline-none px-2 bg-transparent"
+                    className="flex-1 w-3 outline-none px-2 bg-transparent"
                     type="text"
                     name="buscar"
                     value={buscar.buscar} // Corregido: .buscar
@@ -32,11 +32,12 @@ const HederDashboardAliado = () => {
                 />
             </div>
 
-            <div className="flex gap-4 items-center">
-                <button className="p-2 bg-white rounded-full hover:bg-gray-100 transition-colors">
+            <div className="flex gap-2 justify-content items-center">
+                <button className="flex justify-center items-center bg-white rounded-full cursor-pointer size-10
+                hover:bg-gray-100 transition-colors">
                     <img src={Campana} alt="campana" className="w-6 h-6" />
                 </button>
-                <button className="overflow-hidden rounded-full border-2 border-white shadow-sm">
+                <button className="overflow-hidden rounded-full border-2 cursor-pointer border-white shadow-sm">
                     <img className="w-10 h-10 object-cover" src={FotoPerfil} alt="perfil" />
                 </button>
             </div>
