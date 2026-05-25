851da8cf53b16fb35cd6233041caa4cd1670d3b6
Camilo <bermeocamilo12@gmail.com>
2026-05-25 12:42:15 -0500

    fix(test):init test for project

---
+++ backEnd/src/test/java/com/fasterEnvios/application/useCase/payment/PaymentUseCaseTest.java
@@ -0,0 +1,13 @@
+package com.fasterEnvios.application.useCase.payment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentUseCaseTest {
+
+    @Test
+    void execute() {
+
+    }
+}
