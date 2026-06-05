89d50ff4fe8c502329ef4aa8e3a019cddae6a2f6
Camilo <bermeocamilo12@gmail.com>
2026-06-05 16:13:36 -0500

    refactor(userEntity):Refactor user entity to match the use case

--- backEnd/src/main/java/com/fasterEnvios/infrastructure/entity/UserEntity.java
+++ backEnd/src/main/java/com/fasterEnvios/infrastructure/entity/UserEntity.java
@@ -17,6 +17,14 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String phoneNumber;
+    @Column(nullable = false)
+    private String identityDocument;
     @Column(nullable = false,  unique = true)
     private String email;
     @Column(nullable = false,  unique = true)
