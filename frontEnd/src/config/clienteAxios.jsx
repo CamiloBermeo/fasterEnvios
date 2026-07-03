9936c42531561df0e1fa16d22c655d076e105dc4
Camilo <bermeocamilo12@gmail.com>
2026-07-03 11:49:48 -0500

    added new version url

--- frontEnd/src/config/clienteAxios.jsx
+++ frontEnd/src/config/clienteAxios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://faster-envio-alb-115006143.us-east-1.elb.amazonaws.com'
+const API_URL = import.meta.env.VITE_API_URL || 'http://faster-envio-alb-115006143.us-east-1.elb.amazonaws.com/api/v1'
 
 const clienteAxios = axios.create({
     baseURL: API_URL
