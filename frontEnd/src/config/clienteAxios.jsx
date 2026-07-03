febf0160e07a01d9aa66456ab1510ca559961b52
Camilo <bermeocamilo12@gmail.com>
2026-07-03 11:41:13 -0500

    configuration url of production

--- frontEnd/src/config/clienteAxios.jsx
+++ frontEnd/src/config/clienteAxios.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://faster-envio-alb-115006143.us-east-1.elb.amazonaws.com'
+
 const clienteAxios = axios.create({
-    baseURL: "http://localhost:8080/api/v1"
+    baseURL: API_URL
 });
 
 export default clienteAxios;
