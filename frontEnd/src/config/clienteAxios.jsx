cb9a3814d8c828f8d56685193196b8073d611018
Camilo <bermeocamilo12@gmail.com>
2026-07-08 16:06:12 -0500

    fixed(clientAxios): fixed url in the clientAxios for a conection succes

--- frontEnd/src/config/clienteAxios.jsx
+++ frontEnd/src/config/clienteAxios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://faster-envios-backend.up.railway.app/'
+const API_URL = import.meta.env.VITE_API_URL || 'https://faster-envios-backend.up.railway.app/api/v1'
 
 const clienteAxios = axios.create({
     baseURL: API_URL
