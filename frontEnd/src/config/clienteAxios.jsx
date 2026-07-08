26c964627ff83a9d3d2c563be04256dcc8ce1c72
Camilo <bermeocamilo12@gmail.com>
2026-07-08 15:47:53 -0500

    fix(ClientAxios): fixed the url of the my back deployment in the railway

--- frontEnd/src/config/clienteAxios.jsx
+++ frontEnd/src/config/clienteAxios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://faster-envio-alb-115006143.us-east-1.elb.amazonaws.com/api/v1'
+const API_URL = import.meta.env.VITE_API_URL || 'https://faster-envios-backend.up.railway.app/'
 
 const clienteAxios = axios.create({
     baseURL: API_URL
