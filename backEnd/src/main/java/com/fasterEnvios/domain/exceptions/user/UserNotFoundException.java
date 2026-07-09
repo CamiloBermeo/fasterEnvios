e549a68ef2e7f02b61f56151f8f88498eb8a7f2b
Camilo <bermeocamilo12@gmail.com>
2026-07-09 12:44:39 -0500

    fixed(userNotFoundException): fixed exception response message

--- backEnd/src/main/java/com/fasterEnvios/domain/exceptions/user/UserNotFoundException.java
+++ backEnd/src/main/java/com/fasterEnvios/domain/exceptions/user/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.fasterEnvios.domain.exceptions.user;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String email) {
-        super("el usuario"+email+"no se encuentra registrado en la base de datos ");
+        super("el usuario -"+email+"- no se encuentra registrado en la base de datos ");
     }
 }
